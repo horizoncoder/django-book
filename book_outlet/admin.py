d9c1e7e18575d591f7c3ff9532f278573431f315
Dmytro <dbelimenko.99@gmail.com>
2024-09-10 15:20:19 +0300

    add one-to-one relations

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author, Address
 
 
 # Register your models here.
@@ -8,7 +8,8 @@ class BookAdmin(admin.ModelAdmin):
     # readonly_fields =('slug',)
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ('author', 'rating')
-    list_display = ('author', 'rating')
+    list_display = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,13 +1,24 @@
+
 from django.urls import reverse
 
 from django.db import models
 from django.core.validators import  MinValueValidator, MaxValueValidator
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street},{self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
 
 class Author(models.Model):
    first_name = models.CharField(max_length=100)
    last_name = models.CharField(max_length=100)
-
+   address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
 
    def full_name(self):
