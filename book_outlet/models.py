ae8b6546233d71c11fed5a2df4868fdcc3f5edf8
Dmytro <dbelimenko.99@gmail.com>
2024-09-10 14:16:44 +0300

    config admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,11 +10,12 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True,max_length=128)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
-
-    def save(self, *args, **kwargs):
-        self.slug =slugify(self.title)
-        super().save(*args, **kwargs)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True
+                            )
+    # make slug by Admin
+    # def save(self, *args, **kwargs):
+    #     self.slug =slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title}, ({self.rating})"
